6030d80c2b74a3e2dfce43e8cb497870215010d9
vikas kumar <vkbookworm@gmail.com>
2019-04-22 00:14:05 +0530

    feat: first commit

---
+++ src/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Intro">
+        <p className="h1">Hi!</p>
+        <p className="h2">I am Vikas Kumar</p>
+        <p className="h3">A Android Developer</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ src/Header.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
+import twitter from './twitter.svg';
+import linkedIn from './linkedin.svg';
+import github from './github.svg';
+
+class Header extends Component {
+  render() {
+    return (
+      <div class="columns" className="App-header">
+        <div class="column is-one-third" className="App-logo">
+          vikas.dev
+        </div>
+        <div class="column is-one-third" className="App-menu">
+          <ul>
+            <li>
+              <a href="#home">About</a>
+            </li>
+            <li>
+              <a href="#home">Portfolio</a>
+            </li>
+            <li>
+              <a href="#home">Tech</a>
+            </li>
+            <li>
+              <a href="#home">Blog</a>
+            </li>
+          </ul>
+        </div>
+        <div class="column is-one-third" className="App-social">
+          <ul>
+            <li>
+              <a href="#home">
+                <img src={twitter} height="20" width="20" />
+              </a>
+            </li>
+            <li>
+              <a href="#home">
+                <img src={linkedIn} height="20" width="20" />
+              </a>
+            </li>
+            <li>
+              <a href="#home">
+                <img src={github} height="20" width="20" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
