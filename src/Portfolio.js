509a0671942b87b4eee435912321305ab066b262
vikas kumar <vkbookworm@gmail.com>
2019-05-02 01:21:30 +0530

    feat: added the second row of information

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -10,7 +10,7 @@ class PortFolio extends Component {
         description:
           'Share, discuss and chat on live topics and news. Beautiful feed based on interests and interact with like minded people and win prizes.',
         link: 'https://play.google.com/store/apps/details?id=com.app.manch',
-        lang: 'Java'
+        lang: 'Kotlin/Java'
       },
       Clipper: {
         icon:
@@ -29,6 +29,24 @@ class PortFolio extends Component {
           'Android file picker library written in Kotlin. Pick image from gallery or capture image or video. Available on github ',
         link: 'https://goo.gl/5JSMTh',
         lang: 'Kotlin'
+      },
+      Incred: {
+        icon:
+          'https://lh3.googleusercontent.com/lLwgW0ZxTmxhvCScSVME0zzlNaKxow1pT_1nNVr4JOnaRo6vmjaeUNcms_19toM0yas=s360-rw',
+        title: 'Incred Customer',
+        description:
+          'Now apply for InCred Personal Loans and Manage all your InCred Loan accounts using the InCred App.',
+        link: 'https://goo.gl/gy8UCn',
+        lang: 'Java'
+      },
+      IncredSales: {
+        icon:
+          'https://lh3.googleusercontent.com/Bagw1CM0hQq7kiU_pnZTOciMlWUB1Qs6PtaOuHQtiuEAGQF1G3cOjcD6lT2KNHEW7uQ_=s360-rw',
+        title: 'Incred Sales',
+        description:
+          "InCred's sales app for Two Wheeler Loans. To be used by sales executives to apply for new consumer loans.",
+        link: 'https://play.google.com/store/apps/details?id=com.incred.sales',
+        lang: 'Java'
       }
     }
   };
@@ -144,6 +162,82 @@ class PortFolio extends Component {
                 <div class="action has-text-centered" className="Action-card">
                   <a
                     href={this.state.projects.FilePicker.link}
+                    class="button is-success is-rounded  is-small"
+                    target="_blank"
+                  >
+                    Visit
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="columns is-mobile is-centered is-full"
+          className="scrolling-wrapper-flexbox"
+        >
+          <div class="column is-one-quarter" className="Section">
+            <div class="card" className="Card">
+              <div class="header" className="Header-card">
+                <div class="avatar" className="Avatar-card">
+                  <img
+                    className="Img-card"
+                    src={this.state.projects.Incred.icon}
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div class="card-body" className="Card-body">
+                <div class="user-meta has-text-centered" className="User-meta">
+                  <h3 class="username" className="Username-card">
+                    {this.state.projects.Incred.title}
+                  </h3>
+                  <h5 class="position" className="Position-card">
+                    {this.state.projects.Incred.lang}
+                  </h5>
+                </div>
+                <div class="user-bio has-text-centered" className="User-bio">
+                  <p>{this.state.projects.Incred.description}</p>
+                </div>
+                <div class="action has-text-centered" className="Action-card">
+                  <a
+                    href={this.state.projects.Incred.link}
+                    class="button is-success is-rounded  is-small"
+                    target="_blank"
+                  >
+                    Visit
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="column is-one-quarter" className="Section">
+            <div class="card" className="Card">
+              <div class="header" className="Header-card">
+                <div class="avatar" className="Avatar-card">
+                  <img
+                    className="Img-card"
+                    src={this.state.projects.IncredSales.icon}
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div class="card-body" className="Card-body">
+                <div class="user-meta has-text-centered" className="User-meta">
+                  <h3 class="username" className="Username-card">
+                    {this.state.projects.IncredSales.title}
+                  </h3>
+                  <h5 class="position" className="Position-card">
+                    {this.state.projects.IncredSales.lang}
+                  </h5>
+                </div>
+                <div class="user-bio has-text-centered" className="User-bio">
+                  <p>{this.state.projects.IncredSales.description}</p>
+                </div>
+                <div class="action has-text-centered" className="Action-card">
+                  <a
+                    href={this.state.projects.IncredSales.link}
                     class="button is-success is-rounded  is-small"
                     target="_blank"
                   >
