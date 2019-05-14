4b31807588e8bdbb4920b44a4fae4567b80a3155
vikas <vkbookworm@gmail.com>
2019-05-14 22:09:47 +0530

    fix: spell fix

--- src/Content.js
+++ src/Content.js
@@ -7,7 +7,7 @@ class About extends Component {
         <p className="h1">Hi!</p>
         <p className="h2">I am Vikas Kumar</p>
         <p className="h3">Android Developer</p>
-        <p className="h4">A self thought android developer and designer.</p>
+        <p className="h4">A self taught android developer and designer.</p>
         <p className="h4">
           Enjoy writing clean code and contributing to Open source.
         </p>
